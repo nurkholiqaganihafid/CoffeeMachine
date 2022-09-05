c42b87bebe6ed958f521a4353f9f9de6acf40020
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2022-09-06 01:35:24 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Menyalakan mesin kopi");
+console.log("Menggiling biji kopi");
+console.log("Memanaskan air");
+console.log("Mencampurkan air dan kopi");
+console.log("Menuangkan kopi kedalam gelas");
+console.log("Menuangkan susu kedalam gelas");
+console.log("Kopi Anda sudah siap!")
