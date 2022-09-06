f62a9c135abfae54589500d6c220aad111aebeff
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2022-09-06 15:09:54 +0700

    implement ES6 Module

---
+++ animal.js
@@ -0,0 +1,10 @@
+const hewan = {
+    cow: "Sapi",
+    cat: "Kucing",
+    dog: "Anjing",
+    goat: "Kambing"
+}
+
+const isFourLegs = true
+
+export { hewan, isFourLegs }
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import { hewan, isFourLegs as berkakiEmpat } from './animal.js';
+// const { coffeeStock, isCoffeeMachineReady } = require('./state')
+
 console.log("Menyalakan mesin kopi");
 console.log("Menggiling biji kopi");
 console.log("Memanaskan air");
@@ -6,21 +9,33 @@ console.log("Menuangkan kopi kedalam gelas");
 console.log("Menuangkan susu kedalam gelas");
 console.log("Kopi Anda sudah siap!")
 
-const {coffeeStock, isCoffeeMachineReady} = require('./state')
+// const makeCoffee = (type, miligrams) => {
+//     if (coffeeStock[type] >= miligrams) {
+//         console.log("Kopi berhasil dibuat");
+//     } else {
+//         console.log("Biji kopi habis!");
+//     }
+// }
 
-const makeCoffee = (type, miligrams) => {
-    if (coffeeStock[type] >= miligrams) {
-        console.log("Kopi berhasil dibuat");
-    } else {
-        console.log("Biji kopi habis!");
+// console.log();
+// makeCoffee("arabica", 300)
+
+// console.log();
+// console.log(coffeeStock);
+
+// console.log();
+// console.log(isCoffeeMachineReady);
+
+const animalName = name => {
+    for (const type in name) {
+        console.log(type);
     }
 }
 
 console.log();
-makeCoffee("arabica", 300)
+animalName(hewan)
 
 console.log();
-console.log(coffeeStock);
+console.log(hewan);
 
-console.log();
-console.log(isCoffeeMachineReady);
+console.log(`Hewan berkaki empat ${berkakiEmpat}`);
