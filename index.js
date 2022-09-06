995de322a68f683355ddcb245ae0482454b534de
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2022-09-06 12:23:17 +0700

    export multiple values in Node.js

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ console.log("Mencampurkan air dan kopi");
 console.log("Menuangkan kopi kedalam gelas");
 console.log("Menuangkan susu kedalam gelas");
 console.log("Kopi Anda sudah siap!")
+
+const {coffeeStock, isCoffeeMachineReady} = require('./state')
+
+const makeCoffee = (type, miligrams) => {
+    if (coffeeStock[type] >= miligrams) {
+        console.log("Kopi berhasil dibuat");
+    } else {
+        console.log("Biji kopi habis!");
+    }
+}
+
+console.log();
+makeCoffee("arabica", 300)
+
+console.log();
+console.log(coffeeStock);
+
+console.log();
+console.log(isCoffeeMachineReady);
